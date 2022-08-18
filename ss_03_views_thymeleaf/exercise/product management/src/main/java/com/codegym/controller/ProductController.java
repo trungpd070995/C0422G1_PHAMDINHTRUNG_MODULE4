322c24a7741437dcce7713851158054ed17de995
unknown <you@example.com>
2022-08-18 19:44:07 +0700

    up

---
+++ ss_03_views_thymeleaf/exercise/product management/src/main/java/com/codegym/controller/ProductController.java
@@ -0,0 +1,79 @@
+package com.codegym.controller;
+
+import com.codegym.model.Product;
+import com.codegym.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private IProductService productService;
+
+    @GetMapping("")
+    public String index(Model model) {
+        List<Product> productList = productService.findAll();
+        model.addAttribute("products", productList);
+        return "/index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("product", new Product());
+        return "/create";
+    }
+
+    @PostMapping("/save")
+    public String edit(Product product) {
+        product.setId((int) (Math.random() * 1000));
+        productService.save(product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        return "/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Product product) {
+        productService.update(product.getId(), product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable int id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        return "/delete";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Product product, RedirectAttributes redirectAttributes) {
+        productService.remove(product.getId());
+        redirectAttributes.addFlashAttribute("success", "Removed customer successfully!");
+        return "redirect:/product";
+    }
+
+    @GetMapping("/{id}/view")
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        return "/view";
+    }
+
+    @PostMapping("/search")
+    public String findByName(String name, Model model) {
+        model.addAttribute("products", productService.findByName(name));
+        return "/index";
+    }
+}
---
+++ ss_03_views_thymeleaf/exercise/product management/src/main/java/com/codegym/repository/ProductRepositoryImpl.java
@@ -0,0 +1,59 @@
+package com.codegym.repository;
+
+import com.codegym.model.Product;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ProductRepositoryImpl implements IProductRepository {
+
+    private static final Map<Integer, Product> products;
+
+    static {
+        products = new HashMap<>();
+        products.put(1, new Product(1, "XiaoMi", 100, "Còn hàng", "Mi"));
+        products.put(2, new Product(2, "Iphone", 200, "Hết hàng", "Apple"));
+        products.put(3, new Product(3, "Galaxy", 300, "Còn hàng", "Samsung"));
+        products.put(4, new Product(4, "Q-mobile", 400, "Hết hàng", "Q"));
+        products.put(5, new Product(5, "Realme", 500, "Còn hàng", "Xiaomi"));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
+    }
+
+    @Override
+    public void save(Product product) {
+        products.put(product.getId(), product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        return products.get(id);
+    }
+
+    @Override
+    public void update(int id, Product product) {
+        products.put(id, product);
+    }
+
+    @Override
+    public void remove(int id) {
+        products.remove(id);
+    }
+
+    @Override
+    public List<Product> findByName(String name) {
+        List<Product> products = new ArrayList<>();
+        for (Product product : products) {
+            if (product.getName().contains(name)) {
+                products.add(product);
+            }
+        }
+        return products;
+    }
+}
