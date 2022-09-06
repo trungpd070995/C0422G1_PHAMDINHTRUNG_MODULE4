9bcb4702d8a9939ccc0a12619c3760080144f581
unknown <you@example.com>
2022-09-06 19:14:33 +0700

    up

---
+++ ss_14_web_application _security/pratice/spring-security/src/main/java/com/example/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class AppConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+}
